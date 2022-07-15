a4d4a9b012b16fd9f9467be3ba560040e4405421
Tony <toekneegaming123@gmail.com>
2022-07-15 19:58:10 -0400

    Both Modals' OK buttons are AcceptButtons allowing for Enter key submission

--- ModalDialog.Designer.cs
+++ ModalDialog.Designer.cs
@@ -68,6 +68,7 @@ namespace AnthonySeymourGOL
             // 
             // ModalDialog
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(259, 66);
